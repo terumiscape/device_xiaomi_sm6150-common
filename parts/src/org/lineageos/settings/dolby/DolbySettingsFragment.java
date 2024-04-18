aosp-playground/android_device_xiaomi_sm6150-common

sm6150-common: parts: Introduce Dolby Atmos
Based on existing dirac implementation and observing stock
sound effects app and daxservice.

Thanks to jhenrique09 for the hack from old dirac parts
"Pause/play music stream to get effects applied".

TODO: bring back misound (same as stock)

Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
Change-Id: I528a8c90defe336dcb79fa1c31d3d52b9d39fe14

parts/src/org/lineageos/settings/dolby/DolbySettingsFragment.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2018,2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.dolby;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Switch;
+
+import androidx.preference.Preference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+import com.android.settingslib.widget.OnMainSwitchChangeListener;
+
+import org.lineageos.settings.R;
+
+public class DolbySettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener, OnMainSwitchChangeListener {
+
+    public static final String PREF_ENABLE = "dolby_enable";
+    public static final String PREF_PRESET = "dolby_preset";
+    public static final String PREF_PROFILE = "dolby_profile";
+
+    private MainSwitchPreference mSwitchBar;
+    private ListPreference mPresetPref;
+    private ListPreference mProfilePref;
+
+    private DolbyUtils mDolbyUtils;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.dolby_settings);
+
+        mDolbyUtils = DolbyUtils.getInstance(getActivity());
+        final boolean dsOn = mDolbyUtils.getDsOn();
+
+        mSwitchBar = (MainSwitchPreference) findPreference(PREF_ENABLE);
+        mSwitchBar.addOnSwitchChangeListener(this);
+        mSwitchBar.setChecked(dsOn);
+
+        mPresetPref = (ListPreference) findPreference(PREF_PRESET);
+        mPresetPref.setOnPreferenceChangeListener(this);
+        mPresetPref.setEnabled(dsOn);
+
+        mProfilePref = (ListPreference) findPreference(PREF_PROFILE);
+        mProfilePref.setOnPreferenceChangeListener(this);
+        mProfilePref.setEnabled(dsOn);
+        mProfilePref.setValue(Integer.toString(mDolbyUtils.getProfile()));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference.getKey()) {
+            case PREF_PRESET:
+                mDolbyUtils.setPreset(newValue.toString());
+                return true;
+            case PREF_PROFILE:
+                mDolbyUtils.setProfile(Integer.parseInt((newValue.toString())));
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        mSwitchBar.setChecked(isChecked);
+
+        mDolbyUtils.setDsOn(isChecked);
+        mPresetPref.setEnabled(isChecked);
+        mProfilePref.setEnabled(isChecked);
+    }
+}

terumiscape/device_xiaomi_sm6150-common

Signed-off-by: basamaryan <basam.aryan@gmail.com>

parts/src/org/lineageos/settings/dolby/DolbySettingsFragment.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2018,2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.dolby;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import androidx.preference.Preference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+
+import org.lineageos.settings.R;
+
+public class DolbySettingsFragment extends PreferenceFragment implements
+        OnPreferenceChangeListener, OnCheckedChangeListener {
+
+    public static final String PREF_ENABLE = "dolby_enable";
+    public static final String PREF_PRESET = "dolby_preset";
+    public static final String PREF_PROFILE = "dolby_profile";
+
+    private MainSwitchPreference mSwitchBar;
+    private ListPreference mPresetPref;
+    private ListPreference mProfilePref;
+
+    private DolbyUtils mDolbyUtils;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.dolby_settings);
+
+        mDolbyUtils = DolbyUtils.getInstance(getActivity());
+        final boolean dsOn = mDolbyUtils.getDsOn();
+
+        mSwitchBar = (MainSwitchPreference) findPreference(PREF_ENABLE);
+        mSwitchBar.addOnSwitchChangeListener(this);
+        mSwitchBar.setChecked(dsOn);
+
+        mPresetPref = (ListPreference) findPreference(PREF_PRESET);
+        mPresetPref.setOnPreferenceChangeListener(this);
+        mPresetPref.setEnabled(dsOn);
+
+        mProfilePref = (ListPreference) findPreference(PREF_PROFILE);
+        mProfilePref.setOnPreferenceChangeListener(this);
+        mProfilePref.setEnabled(dsOn);
+        mProfilePref.setValue(Integer.toString(mDolbyUtils.getProfile()));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference.getKey()) {
+            case PREF_PRESET:
+                mDolbyUtils.setPreset(newValue.toString());
+                return true;
+            case PREF_PROFILE:
+                mDolbyUtils.setProfile(Integer.parseInt((newValue.toString())));
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        mSwitchBar.setChecked(isChecked);
+
+        mDolbyUtils.setDsOn(isChecked);
+        mPresetPref.setEnabled(isChecked);
+        mProfilePref.setEnabled(isChecked);
+    }
+}